680b49da0de975ebd26cc477229914db64821ae5
Affanxali10 <affan.kate@gmail.com>
2024-09-17 18:26:46 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,103 @@
+// Java script Chapter 1 (Assignment : 1)
+// alert("Hello everybody, Welcome to the JS practice page")
+// var userInput = prompt("What is your name?"); // Ask the user for input
+// alert("Nice to meet you, " + userInput + "!"); // Display the input in an alert
+
+
+// Java script Chapter 2 (Assignment : 2)
+// a = "Affan";
+// b = " Ali";
+// c = a + b;
+// console.log(c);
+
+// Java script Chapter 3 (Assignment : 3)
+// var a = 25;
+// let b = a * 3;
+// console.log(b)
+
+// var weight = 200;
+// weight = weight + 50;
+// console.log(weight + 50)
+
+// Java script Chapter 4 (Assignment : 4)
+// legal:
+// myname = "Affan"
+// my_name = "Affan"
+// _myname = "Affan"
+// MyName = "Affan"
+// MYNAME = "Affan"
+// myname2 = "Affan"
+// myname$ = "Affan"
+// illegal:
+// 2myname = "Affan"
+// my - name = "Affan"
+// my name = "Affan"
+// my @name = "Affan"
+// my % name = "Affan"
+
+// Java script Chapter 5 (Assignment : 5)
+// var a = 55
+// var b = 5
+// console.log(a - b)
+// var a = 55
+// var b = 5
+// console.log(a + b)
+// var a = 55
+// var b = 5
+// console.log(a * b)
+// var a = 55
+// var b = 5
+// console.log(a / b)
+
+// Java script Chapter 6 (Assignment : 6)
+// var a = 2
+// var b = 44
+// console.log(a-- b)
+// var a = 2
+// var b = 44
+// console.log(a - + b)
+// var a = 2
+// var b = 44
+// console.log(a++ b)
+
+// Java script Chapter 7 (Assignment : 7)
+// var a = 55
+// var b = 5
+// console.log(2 * a + b)
+// var a = 55
+// var b = 5
+// console.log(15 * a + b)
+
+// // Java script Chapter 8 (Assignment : 8)
+// var name = "Affan";
+// alert("Thanks, " + name + "!")
+
+// Java script Chapter 9 (Assignment : 9)
+// var question = "Your Name?";
+// var spec = prompt(question)
+
+// Simple Calculator Using Promt
+// var msg1 = prompt("Enter your 1st Number");
+// var msg3 = Number(msg1);
+// var msg2 = prompt("Enter your 2nd Number");
+// var msg4 = Number(msg2);
+// alert(msg3 + msg4);
+// alert("Welcome Buddy!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
